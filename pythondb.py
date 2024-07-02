b91716cfc4b25ff93e3fe1caf49dd47d4930436e
Pål Egil Runde <paal.runde@gmail.com>
2024-07-02 15:27:17 +0200

    added delete functionality

--- pythondb.py
+++ pythondb.py
@@ -21,8 +21,8 @@ def insert_query(word, translation):
 
     dbconn = db_connection()
     cursor = dbconn.cursor()
-    insert_query = "INSERT INTO dictionary (word, translation) VALUES (%s, %s)"
-    cursor.execute(insert_query, (word, translation))
+    insert_script = "INSERT INTO dictionary (word, translation) VALUES (%s, %s)"
+    cursor.execute(insert_script, (word, translation))
     dbconn.commit()
     cursor.close()
     dbconn.close()
@@ -33,6 +33,23 @@ def add_word():
     insert_query(word, translation)
     print(f'Added {word} with translation: {translation} to dictionary')
 
+def delete_query(word): 
+    dbconn = db_connection()
+    cursor = dbconn.cursor()
+    delete_script = 'DELETE FROM dictionary WHERE word = %s'
+    #added ',' at end of word due to needing to pass tuple even if only one argument
+    cursor.execute(delete_script, (word,))
+    dbconn.commit()
+    cursor.close()
+    dbconn.close()
+
+def delete_word():
+    word = input("Enter english word you wish to remove: ")
+    delete_query(word)
+    print(f'Removed {word} from dictionary')
+
+
+
 while True: ## REPL - Read Execute Program Loop
     cmd = input("Command: ")
 
@@ -40,6 +57,8 @@ while True: ## REPL - Read Execute Program Loop
         read_dict()
     elif cmd == "add_word":
         add_word()
+    elif cmd == "delete":
+        delete_word()
     elif cmd == "quit":
         exit()
     else:
