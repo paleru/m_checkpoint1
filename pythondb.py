fed5d35702dacb6d5089c7024fc9f5eed97db0c6
Pål Egil Runde <paal.runde@gmail.com>
2024-07-02 15:18:53 +0200

    added add_word functionality

--- pythondb.py
+++ pythondb.py
@@ -17,12 +17,29 @@ def read_dict():
     dbconn.close()
     print(rows)
 
+def insert_query(word, translation):
+
+    dbconn = db_connection()
+    cursor = dbconn.cursor()
+    insert_query = "INSERT INTO dictionary (word, translation) VALUES (%s, %s)"
+    cursor.execute(insert_query, (word, translation))
+    dbconn.commit()
+    cursor.close()
+    dbconn.close()
+
+def add_word():
+    word = input("Enter english word: ")
+    translation = input("Enter your translation: ")
+    insert_query(word, translation)
+    print(f'Added {word} with translation: {translation} to dictionary')
 
 while True: ## REPL - Read Execute Program Loop
     cmd = input("Command: ")
 
     if cmd == "list":
         read_dict()
+    elif cmd == "add_word":
+        add_word()
     elif cmd == "quit":
         exit()
     else:
