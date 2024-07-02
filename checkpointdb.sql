01b8da56d99bb6a368b1826b0a4f30d2c9a45560
Pål Egil Runde <paal.runde@gmail.com>
2024-07-02 14:21:58 +0200

    added checkpoint to main

---
+++ checkpointdb.sql
@@ -0,0 +1,21 @@
+/* 
+1.2Create the database definitions described in the ER-diagram below on the
+next page.
+1.3Assign appropriate data types to each column. Bonus points are given if you
+show different constraints as well.
+1.4Add data from the four sample tables below.
+1.5Add one more row into the CONTACTS table, which should contain your own
+name. Add yourself with a (fake) contact in the ITEMS table as well.
+1.6Create a query that lists if there are unused contact_types.
+1.7Create a VIEW and name it view_contacts that lists the columns:
+first_name, last_name, contact, contact_type, contact_category.
+1.8Create a query that lists all information from the database in one big
+resulting table. The id columns should be invisible in the result.
+Module #4 SQL
+Mastery Checkpoint #1
+M4-D6.1-Mastery_Checkpoint#1-SQL.docx 3(7) 2024-07-02
+1.9Make a comment in the script file about how an alternative solution to the
+design of the ITEMS table would look like in terms of SQL definition code. */
+
+-- 1.1Create a script file named checkpointdb.sql and put it in MC1. Collect all
+-- your SQL statements in this .sql file and make the whole script re-runnable.
